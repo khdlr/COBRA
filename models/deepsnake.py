693b966f6124654767b5f7b0aedea24cbf323db7
Konrad Heidler <k.heidler@tum.de>
2022-02-07 19:39:19 +0100

    No Weight Sharing

--- models/deepsnake.py
+++ models/deepsnake.py
@@ -9,7 +9,8 @@ from . import snake_utils
 
 class DeepSnake():
     def __init__(self, backbone, vertices=64,
-            model_dim=64, iterations=5, head='SnakeHead', coord_features=False, stop_grad=True):
+            model_dim=64, iterations=5, head='SnakeHead', coord_features=False,
+            weight_sharing=True, stop_grad=True):
         super().__init__()
         self.backbone = getattr(backbones, backbone)
         self.model_dim = model_dim
@@ -17,6 +18,7 @@ class DeepSnake():
         self.coord_features = coord_features
         self.vertices = vertices
         self.stop_grad = stop_grad
+        self.weight_sharing = weight_sharing
         self.head = head
 
     def __call__(self, imagery, is_training=False):
@@ -29,8 +31,12 @@ class DeepSnake():
         vertices = jnp.zeros([imagery.shape[0], self.vertices, 2])
         steps = [vertices]
 
-        _head = getattr(snake_utils, self.head)(self.model_dim, self.coord_features)
-        head = lambda x, y: _head(x, y)
+        if self.weight_sharing:
+          _head = getattr(snake_utils, self.head)(self.model_dim, self.coord_features)
+          head = lambda x, y: _head(x, y)
+        else:
+          head_cls = getattr(snake_utils, self.head)
+          head = lambda x, y: head_cls(self.model_dim, self.coord_features)(x, y)
 
         for _ in range(self.iterations):
             if self.stop_grad:
