d3dd3439c34ae603fd21043454aa3d245de273b0
Konrad Heidler <k.heidler@tum.de>
2023-04-19 13:46:30 +0200

    Add inference map

---
+++ content/map.md
@@ -0,0 +1,6 @@
++++
+title = "Inference Map"
+draft = false
++++
+
+<iframe src="./map.html" style="width:100%;height:80vh;"></iframe>
---
+++ inference_map/main.js
@@ -0,0 +1,110 @@
+import Map from 'ol/Map';
+import View from 'ol/View';
+import Group from 'ol/layer/Group.js';
+import TileLayer from 'ol/layer/Tile';
+import WMTS from 'ol/source/WMTS.js';
+import WMTSTileGrid from 'ol/tilegrid/WMTS.js';
+import GeoJSON from 'ol/format/GeoJSON';
+import {get as getProjection, transform} from 'ol/proj.js';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import {getTopLeft, getWidth} from 'ol/extent.js';
+import {Style, Text, Fill, Stroke, Icon} from 'ol/style.js';
+import {Attribution, defaults as defaultControls} from 'ol/control.js';
+import 'ol-layerswitcher/dist/ol-layerswitcher.css';
+import LayerSwitcher from 'ol-layerswitcher';
+
+window.show_story = function() {
+  document.getElementById('story').classList.add("active");
+}
+
+window.hide_story = function() {
+  document.getElementById('story').classList.remove("active");
+}
+
+
+const projection = getProjection('EPSG:3857');
+console.log(projection.getExtent())
+const size = getWidth(projection.getExtent()) / 256;
+const resolutions = new Array(19);
+const matrixIds = new Array(19);
+for (let z = 0; z < 19; ++z) {
+  // generate resolutions and matrixIds arrays for this WMTS
+  resolutions[z] = size / Math.pow(2, z);
+  matrixIds[z] = z;
+}
+
+const s2_source = new WMTS({
+  attributions: `Base Layer: <a class="a-light" xmlns:dct="http://purl.org/dc/terms/" href="https://s2maps.eu" property="dct:title">Sentinel-2 cloudless - https://s2maps.eu</a> by <a class="a-light" xmlns:cc="http://creativecommons.org/ns#" href="https://eox.at" property="cc:attributionName" rel="cc:attributionURL">EOX IT Services GmbH</a> (Contains modified Copernicus Sentinel data 2021)`,
+  url: 'https://s2maps-tiles.eu/wmts/',
+  maxZoom: 18,
+  layer: 's2cloudless-2020_3857',
+  matrixSet: 'GoogleMapsCompatible',
+  projection: projection,
+  format: 'image/jpeg',
+  style: 'default',
+  wrapX: true,
+  tileGrid: new WMTSTileGrid({
+    origin: getTopLeft(projection.getExtent()),
+    resolutions: resolutions,
+    matrixIds: matrixIds,
+  })
+});
+
+// Create the basemap layer
+const s2_layer = new TileLayer({
+  source: s2_source,
+});
+
+const calfin_style = new Style({stroke: new Stroke({color: 'red', width: 3})});
+const calfin_layer = new VectorLayer({
+  source: new VectorSource({
+    url: 'geojson/CALFIN.geo.json',
+    format: new GeoJSON({}),
+  }),
+  style: calfin_style,
+  title: '<span style="color:red;font-weight:bold;">—</span> Calfin Predictions'
+});
+
+const tud_style = new Style({stroke: new Stroke({color: 'blue', width: 3})});
+const tud_layer = new VectorLayer({
+  source: new VectorSource({
+    url: 'geojson/TUD.geo.json',
+    format: new GeoJSON({}),
+  }),
+  style: tud_style,
+  title: '<span style="color:blue;font-weight:bold;">—</span> TUD Predictions'
+});
+
+const baumhoer_style = new Style({stroke: new Stroke({color: 'green', width: 3})});
+const baumhoer_layer = new VectorLayer({
+  source: new VectorSource({
+    url: 'geojson/Baumhoer.geo.json',
+    format: new GeoJSON({}),
+  }),
+  style: baumhoer_style,
+  title: '<span style="color:green;font-weight:bold;">&mdash;</span> Baumhoer Predictions'
+});
+
+const attribution = new Attribution({collapsible: false});
+const layer_switcher = new LayerSwitcher({
+  startActive: true,
+  reverse: false,
+  activationMode: 'click',
+})
+
+// Create the map
+const map = new Map({
+  target: 'map',
+  layers: [
+    s2_layer,
+    calfin_layer,
+    tud_layer,
+    baumhoer_layer
+  ],
+  controls: defaultControls({attribution: false}).extend([attribution, layer_switcher]),
+  view: new View({
+    center: transform([-45,75], 'EPSG:4326', projection),
+    zoom: 5
+  })
+});
